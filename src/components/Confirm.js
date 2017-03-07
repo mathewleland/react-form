9ad261a9abaac515a6f22bb3d3ec0f5a1d0ca81a
mathewleland <mathewhleland@gmail.com>
2017-03-06 16:29:27 -0800

    include tbody into table and default the plan to read Engage if no plan was selected, like when someone may be referred

--- src/components/Confirm.js
+++ src/components/Confirm.js
@@ -12,54 +12,56 @@ class Confirm extends React.Component {
       <div className='container'>
         <h2>Confirm your information</h2>
             <table>
-              <tr>
-                <td>First name</td>
-                <td>{localStorage.firstName}</td>
-              </tr>
-              <tr>
-                <td>Last Name</td>
-                <td>{localStorage.lastName}</td>
-              </tr>
-              <tr>
-                <td>Email</td>
-                <td>{localStorage.email}</td>
-              </tr>
-              <tr>
-                <td>Business / Org name</td>
-                <td>{localStorage.businessName}</td>
-              </tr>
-              <tr>
-                <td>Phone number</td>
-                <td>{localStorage.phone}</td>
-              </tr>
-              <tr>
-                <td>Street Address</td>
-                <td>{localStorage.streetAddress}</td>
-              </tr>
-              <tr>
-                <td>City</td>
-                <td>{localStorage.city}</td>
-              </tr>
-              <tr>
-                <td>State</td>
-                <td>{localStorage.usState}</td>
-              </tr>
-              <tr>
-                <td>ZIP</td>
-                <td>{localStorage.zip}</td>
-              </tr>
-              <tr>
-                <td>Website </td>
-                <td>{localStorage.website || "none yet"}</td>
-              </tr>
-              <tr>
-                <td>Industry</td>
-                <td>{localStorage.industry}</td>
-              </tr>
-              <tr>
-                <td>Plan</td>
-                <td>{localStorage.plan}</td>
-              </tr>
+              <tbody>
+                <tr>
+                  <td>First name</td>
+                  <td>{localStorage.firstName}</td>
+                </tr>
+                <tr>
+                  <td>Last Name</td>
+                  <td>{localStorage.lastName}</td>
+                </tr>
+                <tr>
+                  <td>Email</td>
+                  <td>{localStorage.email}</td>
+                </tr>
+                <tr>
+                  <td>Business / Org name</td>
+                  <td>{localStorage.businessName}</td>
+                </tr>
+                <tr>
+                  <td>Phone number</td>
+                  <td>{localStorage.phone}</td>
+                </tr>
+                <tr>
+                  <td>Street Address</td>
+                  <td>{localStorage.streetAddress}</td>
+                </tr>
+                <tr>
+                  <td>City</td>
+                  <td>{localStorage.city}</td>
+                </tr>
+                <tr>
+                  <td>State</td>
+                  <td>{localStorage.usState}</td>
+                </tr>
+                <tr>
+                  <td>ZIP</td>
+                  <td>{localStorage.zip}</td>
+                </tr>
+                <tr>
+                  <td>Website </td>
+                  <td>{localStorage.website || "none yet"}</td>
+                </tr>
+                <tr>
+                  <td>Industry</td>
+                  <td>{localStorage.industry}</td>
+                </tr>
+                <tr>
+                  <td>Plan</td>
+                  <td>{localStorage.plan || "Engage"}</td>
+                </tr>
+              </tbody>
             </table>
 
 
